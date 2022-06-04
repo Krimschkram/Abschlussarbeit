b06844a6195c84198f48dd3a02c9d107c8bc2fd6
Maximilian Koenig <9125@htl.rennweg.at>
2022-06-04 22:23:56 +0200

    Button Klasse erweitert, GUI etwas verändert und die ersten 7 top Buttons eingebaut -> schaue Kommentare

--- src/CLient/NeuerButton.java
+++ src/CLient/NeuerButton.java
@@ -4,12 +4,17 @@ import javafx.scene.control.Button;
 
 public class NeuerButton extends Button {
 
-    int col;
     int row;
+    int col;
     boolean enabled;
-    boolean abc;    // Damit man die oberen Buttons (die eine Aktion ausführen) mit den Unteren unterscheiden kann
+    boolean drückbar;    // Damit man die oberen Buttons (die eine Aktion ausführen) mit den Unteren unterscheiden kann
     int farbe;      // Farbe des Spielstein auf dem Button (1 = Farbe x / 2 = Farbe y / 0 = leer, noch keine Farbe)
 
 
-
+    public NeuerButton(int row, int col, boolean enabled, boolean drückbar) {
+        this.row = row;
+        this.col = col;
+        this.enabled = enabled;
+        this.drückbar = drückbar;
+    }
 }
